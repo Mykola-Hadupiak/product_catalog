b5efbac8766956e1cf4cc1204a1a76ebe2a2f4a0
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2024-01-25 14:55:22 +0200

    basic code from react_phone catalog

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.scss';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+
+export const App: React.FC = () => (
+  <div className="App">
+    <Header />
+
+    <main className="main">
+      <div className="main__container">
+        <Outlet />
+      </div>
+    </main>
+
+    <Footer />
+  </div>
+);
